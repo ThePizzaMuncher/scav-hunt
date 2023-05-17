9696012aa25bc36eab7cd8a2b093c2775a0a4f02
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-05-17 17:24:24 +0200

    v0.1
    Koter analyzer

---
+++ docent/index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+if ($_SESSION["login"] == "true") {
+
+}
+else {
+    header("location: ../");
+}
+*/
+echo <<< main
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../access/js/script.js" defer></script>
+    <title>Docent pagina</title>
+</head>
+<body>
+    <p>x:<span id="x"></span></p>
+    <p>y:<span id="y"></span></p>
+</body>
+</html>
+main;
+?>
