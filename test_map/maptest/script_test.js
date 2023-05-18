6ba8f325157514244285991cdeea90d3b64fd749
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-05-18 19:56:36 +0200

    Alpha v0.4
    Added a copple of things to search for an alternative for google maps api's. I found the program called: openstreetmap.org.

---
+++ test_map/maptest/script_test.js
@@ -0,0 +1,32 @@
+/* Google maps:
+let map;
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+}
+
+window.addEventListener("load", () => {
+    initMap();
+});
+*/
+
+map = new OpenLayers.Map("map");
+map.addLayer(new OpenLayers.Layer.OSM());
+let markers = new OpenLayers.Layer.Markers("Markers");
+map.addLayer(markers);
+
+function updateMap(x, z, zoom, update) {
+    if (update != false) {
+        markers.clearMarkers();
+    }
+    let fromProjection = new OpenLayers.Projection("EPSG:4326");
+    let toProjection = new OpenLayers.Projection("EPSG:900913");
+    let position = new OpenLayers.LonLat(z, x).transform( fromProjection, toProjection);
+    markers.addMarker(new OpenLayers.Marker(position));
+    map.setCenter(position, zoom);
+}
+
+updateMap(53.199848, 5.764583, 16, false);
