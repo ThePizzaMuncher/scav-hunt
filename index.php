9b8990c7a2b278ae550ddb498b12f3757422738f
ValeraZSD <valerazsd@gmail.com>
2023-05-25 11:10:29 +0200

    what could go wrong (flder nqm3 change)

--- index.php
+++ index.php
@@ -3,11 +3,17 @@
 <main id="main">
 
 <!-- ======= About Section ======= -->
-<section id="about" class="about">
- <div class="content">
-<h3>We are nice guys</h3>
-<button>Here is the button</button>
-</div>
+<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
+  <section id="about" class="section-50 d-flex flex-column align-items-center">
+      <div class="container">
+        <div class="row">
+            <div class="content">
+            <h3>We are nice guys!!</h3>
+            <button>Here is the button</button>
+            </div>
+        </div>
+    </div>
+</section> 
 </section> <!-- End About Section -->
 
 <?php include "assets/includes/footer.php"?>
