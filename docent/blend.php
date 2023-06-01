0bdb23f8e0beff4220592cb097438cbe888dc325
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-01 13:47:04 +0200

    blend count test

--- docent/blend.php
+++ docent/blend.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../assets/includes/conn.php');
-$pull = $conn->query('SELECT naam FROM leerling');
+// $pull = $conn->query('SELECT naam FROM leerling');
 
 $nummer = $conn->query('SELECT COUNT(naam) FROM leerling');
 echo $nummer;
