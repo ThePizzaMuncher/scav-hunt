1c460c1338f3bda2ca223d8f601e8cbe12b67e7f
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-01 13:50:09 +0200

    count test update

--- docent/index.php
+++ docent/index.php
@@ -76,7 +76,7 @@ while($row = mysqli_fetch_row($leerlingen))
 }
 
 	echo "</table></div>";
-	echo "<a href='leerling_toevoegen.php'><button>Toevoegen</button></a><a href='logout.php'><button>Log Out</button></a>";
+	echo "<a href='leerling_toevoegen.php'><button>Toevoegen</button></a><a href='logout.php'><button class='button-red'>Log Out</button></a>";
 ?>
 
 </section>
