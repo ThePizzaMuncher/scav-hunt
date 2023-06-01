e0f5dc96a61f9db3477a584d348d33eb2190c31d
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-01 11:37:04 +0200

    sql fix

--- docent/leerling_toevoegen.php
+++ docent/leerling_toevoegen.php
@@ -74,7 +74,7 @@ echo '<div class="container">
 		 
 
  	// save the data to the database
-	$sql_query = "INSERT INTO leerling(ID,naam, opleiding,groep_ID) VALUES ($id,$naam, $opleiding,1)";
+	$sql_query = "INSERT INTO leerling(naam, opleiding,groep_ID) VALUES ($naam, $opleiding,1)";
 
 	$retval = mysqli_query($conn, $sql_query);
    
