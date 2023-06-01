b4fad4e1d8aa8f7210b177a0c75963899de8db61
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-01 12:37:40 +0200

    login fix

--- docent/leerlingToevSend.php
+++ docent/leerlingToevSend.php
@@ -4,8 +4,8 @@ if (isset($_POST["submit"])) {
     if (isset($_POST["klas"]) && isset($_POST["opleiding"])) {
         $klas = htmlspecialchars($_POST["klas"]);
         $op = htmlspecialchars($_POST["opleiding"]);
-        $conn->query("INSERT INTO leerling(naam, opleiding, groep_ID) VALUES ($klas, $op, 1)");
+        $conn->query("INSERT INTO leerling(naam, opleiding, groep_ID) VALUES ('$klas', '$op', 1)");
     }
 }
-header("../docent");
+header("location: ../docent");
 ?>
