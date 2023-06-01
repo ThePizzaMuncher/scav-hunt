4a04bf68d2e3d04c7c211ebd45fa5d566154af11
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-01 10:36:58 +0200

    login fix v0.4

--- login/send.php
+++ login/send.php
@@ -1,31 +1,8 @@
 <?php
 require_once('../assets/includes/conn.php');
 session_start();
-$user = htmlspecialchars($_POST['user']);
-$pw = htmlspecialchars($_POST['pw']);
-$pull = $conn->query('SELECT * FROM docent');
-$checkAdmin = 0;
-$checkDocent = 0;
-while($row = $pull->fetch_assoc()) {
-	if($user == "admin" && $pw == $row["wachtwoord"]) {
-		$checkAdmin = 1;
-		break;
-	}
-	else if($user == $row["naam"] && $pw == $row["wachtwoord"]) {
-		$checkDocent = 1;
-		break;
-	}
-}
-if($checkAdmin == 1) {
-	$_SESSION["docent"] = 1;
-	header('location:../admin');
-	die();
-}
-if ($checkDocent == 1) {
-	$_SESSION["docent"] = 1;
-	header('location:../docent');
-	die();
-}
-//lfb = Login Feedback
-header('location:../login');
+$usr = htmlspecialchars($_POST["user"]);
+$pw = htmlspecialchars($_POST[""])
+$pull = $conn->query("SELECT * FROM docent");
+while ()
 ?>
