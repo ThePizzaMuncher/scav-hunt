f2a68ab7e6833e7c92596fe51637068ab0a4aeb9
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-05 12:38:16 +0200

    theory of relativity

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -10,24 +10,24 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="/~fp245787/kartel/assets/img/android-chrome-512x512.png" rel="icon">
-  <link href="/~fp245787/kartel/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="/~fp241754/scav-hunt/assets/img/android-chrome-512x512.png" rel="icon">
+  <link href="/~fp241754/scav-hunt/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="/~fp245787/kartel/assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="/~fp245787/kartel/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/~fp245787/kartel/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/~fp245787/kartel/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="/~fp245787/kartel/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="/~fp245787/kartel/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <script src="https://kit.fontawesome.com/51b9333b7a.js" crossorigin="anonymous"></script>
 
   <!-- Main CSS File -->
-  <link href="/~fp245787/kartel/assets/css/style.css" rel="stylesheet">
+  <link href="/~fp241754/scav-hunt/assets/css/style.css" rel="stylesheet">
 
 <body>
   <div id="box"> <?php include_once("../../filedir.php") ?> </div>
@@ -39,12 +39,12 @@
     <div class="container">
       <div class="row">
         <div class="col-md-2">
-          <a href="/~fp245787/kartel/index.php"><img src="/~fp245787/kartel/assets/img/ScavHunt.png" alt="" class="img-fluid rounded-circle"></a>
+          <a href="/~fp241754/scav-hunt/index.php"><img src="/~fp241754/scav-hunt/assets/img/ScavHunt.png" alt="" class="img-fluid rounded-circle"></a>
         </div>
 
         <div class="col-md-3">
           <div class="profile">
-            <h1 class="text-light"><a href="/~fp245787/kartel/index.php">Scav Hunt</a></h1>
+            <h1 class="text-light"><a href="/~fp241754/scav-hunt/index.php">Scav Hunt</a></h1>
             <div class="social-links mt-3 text-center">
               <a href="" target="_blank"><i class="fa-brands fa-instagram"></i></a>
               <a href="" target="_blank"><i class="fa-brands fa-youtube"></i></a>
@@ -58,37 +58,37 @@
           <ul class="nav-menu">
             <?php
             if (isset($_SESSION["docent"])) {
-              echo '<li><a href="'.$rootPath.'/index.php" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>';
+              echo '<li><a href="/~fp241754/index.php" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>';
               echo '<li><a href="code-tonen.php"><i class="bx bx-code"></i>Login</a></li>';
               echo '<li><a href="groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
               echo '<li><a href="winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
-              echo '<li><a href="'.$rootPath.'/docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
+              echo '<li><a href="../../docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
               echo '<li><a href="vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
               echo '<li><a href="docent/"><i class="bx bx-user"></i> Docent</a></li>';
-              echo '<li><a href="'.$rootPath.'/#about" class="nav-link scrollto hover-sound"><i class="bx bx-info"></i> About</a></li>';
+              echo '<li><a href="/~fp241754/scav-hunt/#about" class="nav-link scrollto hover-sound"><i class="bx bx-info"></i> About</a></li>';
             } elseif (isset($_SESSION["admin"])) {
-              echo '<li><a href="'.$rootPath.'/index.php" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>';
+              echo '<li><a href="/~fp241754/scav-hunt/index.php" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>';
               echo '<li><a href="code-tonen.php"><i class="bx bx-code"></i>Login</a></li>';
               echo '<li><a href="groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
               echo '<li><a href="winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
-              echo '<li><a href="'.$rootPath.'/docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
+              echo '<li><a href="/~fp241754/scav-hunt/docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
               echo '<li><a href="vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
               echo '<li><a href="docent_toevoegen.php"><i class="bx bx-code"><i class="bx bx-user"></i>Docent toevoegen</a></li>';
-              echo '<li><a href="'.$rootPath.'/#about" class="nav-link scrollto hover-sound"><i class="bx bx-info"></i> About</a></li>';
+              echo '<li><a href="/~fp241754/scav-hunt/#about" class="nav-link scrollto hover-sound"><i class="bx bx-info"></i> About</a></li>';
             } else {
               function default_bar() {
-                echo '<li><a href="'.$rootPath.'/index.php" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>';
-                echo '<li><a href="'.$rootPath.'/#about" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>';
+                echo '<li><a href="/~fp241754/scav-hunt/index.php" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>';
+                echo '<li><a href="/~fp241754/scav-hunt/#about" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>';
               }
 
               if (isset($_SESSION["pagina"])) {
                 $pn = $_SESSION["pagina"];
                 if ($pn == "home") {
-                  echo '<li><a href="'.$rootPath.'/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
-                  echo '<li><a href="'.$rootPath.'/student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
+                  echo '<li><a href="/~fp241754/scav-hunt/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
+                  echo '<li><a href="/~fp241754/scav-hunt/student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
                 } elseif ($pn == "login") {
-                  echo '<li><a href="'.$rootPath.'/" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Home</a></li>';
-                  echo '<li><a href="'.$rootPath.'/#about" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>';
+                  echo '<li><a href="/~fp241754/scav-hunt/" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Home</a></li>';
+                  echo '<li><a href="/~fp241754/scav-hunt/#about" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>';
                 } else {
                   default_bar();
                 }
