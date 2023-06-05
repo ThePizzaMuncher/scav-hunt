dddd9aa34e962062fe4bf4a136d1a42e530f7dbb
ThePizzaMuncher <j.eppinga7509@gmail.com>
2023-06-05 23:01:00 +0200

    did a derp

--- login/send.php
+++ login/send.php
@@ -3,17 +3,15 @@ require_once('../assets/includes/conn.php');
 session_start();
 $usr = htmlspecialchars($_POST['user']);
 $pw = htmlspecialchars($_POST['pw']);
-// $docent = $conn->query('SELECT * FROM docent');
-// $admin = $conn->query('SELECT * FROM admin');
-// while ($row = $docent->fetch_assoc()) {
-while ($row = $conn->query('SELECT * FROM docent')->fetch_assoc()) {
+$docent = $conn->query('SELECT * FROM docent');
+$admin = $conn->query('SELECT * FROM admin');
+while ($row = $docent->fetch_assoc()) {
 	if ($usr == $row['naam'] && $pw == $row['wachtwoord']) {
 		$_SESSION['docent'] = 1;
 		header('location:../docent');
 		die();
 	}
-// } while ($row = $admin->fetch_assoc()) {
-} while ($row = $conn->query('SELECT * FROM docent')->fetch_assoc()) {
+} while ($row = $admin->fetch_assoc()) {
 	if ($usr == $row['naam'] && $pw == $row['wachtwoord']) {
 		$_SESSION['admin'] = 1;
 		header('location:../admin');
