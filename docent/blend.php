eb0f1700f26e95d6a1bc881be400fddaef2dce92
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-05 09:56:26 +0200

    idiot

--- docent/blend.php
+++ docent/blend.php
@@ -4,6 +4,6 @@ $pull = $conn->query('SELECT * FROM leerling');
 $num = mysqli_num_rows($pull);
 echo $num;
 $llid = '18'; $gID = 1;
-$conn->query("UPDATE leerling SET groep_ID='$gID' WHERE id='llid'");
+$conn->query("UPDATE leerling SET groep_ID='$gID' WHERE id='$llid'");
 $pull = $conn->query('SELECT * FROM leerling');
 ?>
