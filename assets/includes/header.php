e1ae9c4b553b7f5b2758d7fc60c01933ffc66d05
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-08 11:42:03 +0200

    tesjt

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -10,19 +10,19 @@
 	<meta content="" name="keywords">
 
 	<!-- Favicons -->
-	<link href="../../assets/img/android-chrome-512x512.png" rel="icon">
-	<link href="../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+	<link href="/~fp241754/scav-hunt/assets/img/android-chrome-512x512.png" rel="icon">
+	<link href="/~fp241754/scav-hunt/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 	<!-- Vendor CSS Files -->
-	<link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
-	<link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-	<link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-	<link href="../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-	<link href="../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+	<link href="/~fp241754/scav-hunt/assets/vendor/aos/aos.css" rel="stylesheet">
+	<link href="/~fp241754/scav-hunt/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/~fp241754/scav-hunt/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="/~fp241754/scav-hunt/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="/~fp241754/scav-hunt/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="/~fp241754/scav-hunt/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
 	<script src="https://kit.fontawesome.com/51b9333b7a.js" crossorigin="anonymous"></script>
 
@@ -31,7 +31,7 @@
 </head>
 
 <body>
-	<div id="box"> <?php include_once("../../filedir.php") ?> </div>
+	<div id="box"> <?php include_once("/~fp241754/scav-hunt/filedir.php") ?> </div>
 	<i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
 
 	<div class="header-box"></div>
@@ -40,7 +40,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-1">
-					<a href="../../"><img src="../img/ScavHunt.png" alt="" class="img-fluid rounded-circle"></a>
+					<a href="/~fp241754/scav-hunt/"><img src="../img/ScavHunt.png" alt="" class="img-fluid rounded-circle"></a>
 				</div>
 
 				<div class="col-md-2">
@@ -61,44 +61,44 @@
 							<?php
 							if (isset($_SESSION['docent'])) {
 								// echo <<< bar
-								echo '<li><a href="../../docent/"><i class="bx bx-user"></i> Docent</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/"><i class="bx bx-user"></i> Docent</a></li>';
 								echo '<li class="dropdown"><a href="#"><i class="bx bx-user"></i> Beheren <i class="bx bx-chevron-down"></i></a>';
 								echo 	'<ul>';
-								echo 		'<li><a href="../../docent/groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
-								echo 		'<li><a href="../../docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
-								echo 		'<li><a href="../../docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
-								echo 		'<li><a href="../../docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
-								echo 		'<li><a href="../../docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
+								echo 		'<li><a href="/~fp241754/scav-hunt/docent/groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
+								echo 		'<li><a href="/~fp241754/scav-hunt/docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
+								echo 		'<li><a href="/~fp241754/scav-hunt/docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
+								echo 		'<li><a href="/~fp241754/scav-hunt/docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
+								echo 		'<li><a href="/~fp241754/scav-hunt/docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
 								if (isset($_SESSION['admin']))
 									echo	'<li><a href="docent_toevoegen.php"><i class="bx bx-user"></i> Docent toevoegen</a></li>';
 								echo 	'</ul>';
 								echo '</li>';
-								echo '<li><a href="../../login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
 								// bar;
 							} else if ($_SESSION['pagina'] == 'home') {
-								echo '<li><a href="../../login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
-								echo '<li><a href="../../student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
 							}
 							
 							/* else if (isset($_SESSION["admin"])) {
-								echo '<li><a href="../../docent/"><i class="bx bx-user"></i> Docent</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/"><i class="bx bx-user"></i> Docent</a></li>';
 								echo '<li class="dropdown"><a href="#"><i class="bx bx-user"></i> Beheren <i class="bx bx-chevron-down"></i></a>';
 								echo '<ul>';
-								echo '<li><a href="../../docent/groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
-								echo '<li><a href="../../docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
-								echo '<li><a href="../../docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
-								echo '<li><a href="../../docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
-								echo '<li><a href="../../docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
 								echo '<li><a href="docent_toevoegen.php"><i class="bx bx-user"></i> Docent toevoegen</a></li>';
 								echo '</ul>';
 								echo '</li>';
-								echo '<li><a href="../../login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
+								echo '<li><a href="/~fp241754/scav-hunt/login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
 							} else {
 								if ($_SESSION['pagina'] == 'home') {
-									echo '<li><a href="../../login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
-									echo '<li><a href="../../student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
+									echo '<li><a href="/~fp241754/scav-hunt/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
+									echo '<li><a href="/~fp241754/scav-hunt/student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
 								} else {
-									echo '<li><a href="../../docent/code-tonen.php"><i class="bx bx-code"></i>Login</a></li>';
+									echo '<li><a href="/~fp241754/scav-hunt/docent/code-tonen.php"><i class="bx bx-code"></i>Login</a></li>';
 								}
 							} */
 							// the below commented code can be deleted if this works
@@ -108,12 +108,12 @@
 							if (isset($_SESSION["pagina"])) {
 							$pn = $_SESSION["pagina"];
 							if ($pn == "home") {
-							echo '<li><a href="../../login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
-							echo '<li><a href="../../student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
+							echo '<li><a href="/~fp241754/scav-hunt/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
+							echo '<li><a href="/~fp241754/scav-hunt/student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
 							} else if ($pn == "login") default_bar();
 							} else default_bar(); */
 							?>
-							<li><a href="../../about.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>
+							<li><a href="/~fp241754/scav-hunt/about.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>
 						</ul>
 					</nav>
 				</div>
--- docent/test/index.php
+++ docent/test/index.php
@@ -1,33 +1,91 @@
-<!doctype html>
-<html lang="en">
- 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
- 
-    <title>CSV in database importeren testje</title> 
-</head>
- 
-<body>
- 
-    <div class="container">
+<?php
+// Load the database configuration file
+include_once '../../assets/includes/conn.php';
+
+// Get status message
+if(!empty($_GET['status'])){
+    switch($_GET['status']){
+        case 'succ':
+            $statusType = 'alert-success';
+            $statusMsg = 'Members data has been imported successfully.';
+            break;
+        case 'err':
+            $statusType = 'alert-danger';
+            $statusMsg = 'Some problem occurred, please try again.';
+            break;
+        case 'invalid_file':
+            $statusType = 'alert-danger';
+            $statusMsg = 'Please upload a valid CSV file.';
+            break;
+        default:
+            $statusType = '';
+            $statusMsg = '';
+    }
+}
+?>
+
+<!-- Display status message -->
+<?php if(!empty($statusMsg)){ ?>
+<div class="col-xs-12">
+    <div class="alert <?php echo $statusType; ?>"><?php echo $statusMsg; ?></div>
+</div>
+<?php } ?>
+
+<div class="row">
+    <!-- Import link -->
+    <div class="col-md-12 head">
+        <div class="float-right">
+            <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm');"><i class="plus"></i> Import</a>
+        </div>
+    </div>
+    <!-- CSV file upload form -->
+    <div class="col-md-12" id="importFrm" style="display: none;">
         <form action="upload.php" method="post" enctype="multipart/form-data">
-            <div class="input-group">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="customFileInput" aria-describedby="customFileInput" name="file">
-                    <label class="custom-file-label" for="customFileInput">Select file</label>
-                </div>
-                <div class="input-group-append">
-                    <input type="submit" name="submit" value="Upload" class="btn btn-primary">
-                </div>
-            </div>
+            <input type="file" name="file" />
+            <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">
         </form>
     </div>
- 
-</body>
- 
-</html>
+
+    <!-- Data list table --> 
+    <table class="table table-striped table-bordered">
+        <thead class="thead-dark">
+            <tr>
+                <th>#ID</th>
+                <th>Naam</th>
+                <th>Opleiding</th>
+                <th>Leerjaar</th>
+                <th>Groep_ID</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        // Get member rows
+        $result = $db->query("SELECT * FROM leerling ORDER BY id DESC");
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+        ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['naam']; ?></td>
+                <td><?php echo $row['opleiding']; ?></td>
+                <td><?php echo $row['leerjaar']; ?></td>
+                <td><?php echo $row['groep_id']; ?></td>
+            </tr>
+        <?php } }else{ ?>
+            <tr><td colspan="5">No member(s) found...</td></tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Show/hide CSV upload form -->
+<script>
+function formToggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+        element.style.display = "block";
+    }else{
+        element.style.display = "none";
+    }
+}
+</script>
