8143cae1fbf4aef973b559075a6e5e5ed398c077
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-08 14:05:12 +0200

    boem

--- docent/test/index.php
+++ docent/test/index.php
@@ -1,37 +1,12 @@
-<?php
-// Load the database configuration file
-include_once '../../assets/includes/conn.php';
+<!DOCTYPE html>
+<html>
+<body>
 
-// Get status message
-if(!empty($_GET['status'])){
-    switch($_GET['status']){
-        case 'succ':
-            $statusType = 'alert-success';
-            $statusMsg = 'Members data has been imported successfully.';
-            break;
-        case 'err':
-            $statusType = 'alert-danger';
-            $statusMsg = 'Some problem occurred, please try again.';
-            break;
-        case 'invalid_file':
-            $statusType = 'alert-danger';
-            $statusMsg = 'Please upload a valid CSV file.';
-            break;
-        default:
-            $statusType = '';
-            $statusMsg = '';
-    }
-}
-?>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
 
-<!-- Display status message -->
-<?php if(!empty($statusMsg)){ ?>
-<div class="col-xs-12">
-    <div class="alert <?php echo $statusType; ?>"><?php echo $statusMsg; ?></div>
-</div>
-<?php } ?>
-    <!-- CSV file upload form -->
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" />
-            <input type="submit" name="importSubmit" value="IMPORT">
-        </form>
+</body>
+</html>
