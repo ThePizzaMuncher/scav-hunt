b7e43970e3d503b8aad440945a144e01a6256497
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-12 13:07:29 +0200

    Edit.php fuckery

--- docent/edit.php
+++ docent/edit.php
@@ -130,19 +130,15 @@ else
 	{
 	    // query db
 	    $id = $_GET['id'];
-	    $result = mysqli_query($conn,"SELECT * FROM leerling WHERE id=$id")
+	    $result = mysqli_query($conn,"SELECT leerling.naam,leerling.leerjaar,leerling.groep_ID,leerling.opleiding_ID,opleiding.id,opleiding_naam FROM leerling INNER JOIN opleiding ON leerling.opleiding_ID = opleiding.id WHERE id=$id")
 	    or die('doet niet'); 
 	    $row = mysqli_fetch_array($result);
 	    // check that the 'id' matches up with a row in the databse
 	    if ($row)
 		{
 		// get data from db
-		$opleidingid = $row['opleiding_ID'];
 		$naam = $row['naam'];
-		$pull = $conn->query("SELECT leerling.naam,leerling.leerjaar,leerling.opleiding_ID,opleiding.id,opleiding.opleiding_naam FROM leerling INNER JOIN opleiding ON leerling.opleiding_ID = opleiding.id WHERE leerling.opleiding_ID = $opleidingid");
-		$row1 = $pull->fetch_assoc();
-		$opleiding_ID = $row1['opleiding_ID'];
-		echo $row1;
+		$opleiding_ID = $row['opleiding_naam'];
 		$leerjaar = $row['leerjaar'];
 		$groep_ID = $row['groep_ID'];
 		// show form
