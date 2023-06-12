bc0082a5bf264fdf53a0a2f3a5b9b6d21634b34d
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-12 11:44:58 +0200

    cannabis

--- assets/js/blender.js
+++ assets/js/blender.js
@@ -1,8 +1,10 @@
 if (document.getElementById("pagina_blender")) {
     document.getElementById("ag").addEventListener("click", () => {
         document.getElementById("amig").style.display = "none";
+        console.log("klik op ag");
     });
     document.getElementById("amig").addEventListener("click", () => {
         document.getElementById("ag").style.display = "none";
+        console.log("klik op amig");
     });
 }
