c2d37d90c6c8b5058945a601ca22573e5467f2ce
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-12 10:59:49 +0200

    as

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -72,7 +72,7 @@
 									echo	'<li><a href="docent_toevoegen.php"><i class="bx bx-user"></i> Docent toevoegen</a></li>';
 								echo 	'</ul>';
 								echo '</li>';
-								echo '<li><a href="/~fp241754/scav-hunt/login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
+								// echo '<li><a href="/~fp241754/scav-hunt/login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
 								// bar;
 							} else if ($_SESSION['pagina'] == 'home') {
 								echo '<li><a href="/~fp241754/scav-hunt/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
