4b3741b389694e3fa614ea18704b87bbca9a4959
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-12 10:46:24 +0200

    fok

--- docent/index.php
+++ docent/index.php
@@ -53,14 +53,14 @@ require_once("../assets/includes/conn.php");
 		$num_rows = mysqli_num_fields($leerlingen);
 		//    echo 'aantal kolommen' . $num_rows;
 
-		$pull = $conn->query("SELECT leerling.naam,leerling.leerjaar,leerling.groep_ID,leerling.opleiding_ID,opleiding.ID,opleiding.naam FROM leerling INNER JOIN opleiding ON leerling.opleiding_ID = opleiding.ID");
+		$pull = $conn->query("SELECT leerling.naam,leerling.leerjaar,leerling.groep_ID,leerling.opleiding_ID,opleiding.ID,opleiding.opleiding_naam FROM leerling INNER JOIN opleiding ON leerling.opleiding_ID = opleiding.ID");
 		while($row = $pull->fetch_assoc()) {
 			echo "<tr>";
 			echo "<td>$row[ID]</td>";
 			echo "<td>$row[naam]</td>";
 			echo "<td>$row[leerjaar]</td>";
 			echo "<td>$row[groep_ID]</td>";
-			echo "<td>$row[opleiding_ID]</td>";
+			echo "<td>$row[opleiding_naam]</td>";
 			echo "<td><a href=\"edit.php?id=$row[ID]\">Bewerk</a></td>";
             echo "<td><a href=\"delete.php?id=$row[ID]\">Verwijder</a></td>";
 			echo "</tr>";
