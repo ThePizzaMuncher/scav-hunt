ec08e692abff5c0989bd315037cf3bf25e804c48
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-14 11:58:42 +0200

    test

--- docent/blend.php
+++ docent/blend.php
@@ -1,9 +1,6 @@
 <?php
 require_once('../assets/includes/conn.php');
-$pull = $conn->query('SELECT * FROM leerling');
-$num = mysqli_num_rows($pull);
-echo $num;
-$llid = '18'; $gID = 1;
-$conn->query("UPDATE leerling SET groep_ID='$gID' WHERE id='$llid'");
-$pull = $conn->query('SELECT * FROM leerling');
+$test = 1;
+$test = +1;
+echo $test;
 ?>
