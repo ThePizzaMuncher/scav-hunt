0fac95be3031f75c7affd473b4f6173f9754e17c
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-14 13:16:19 +0200

    ಠ_ಠ

--- login/send.php
+++ login/send.php
@@ -10,16 +10,14 @@ while ($row = $docent->fetch_assoc()) {
 	if ($usr == $row['naam'] && $pw == $row['wachtwoord']) {
 		$_SESSION['docent'] = $row['ID'];
 		$_SESSION['opleiding'] = $row['opleiding_ID'];
-		if ($row['isAdmin']) $_SESSION['admin'] = 1;
+		if ($row['isAdmin'] == "1") {
+			$_SESSION['admin'] = 1;
+		}
 		header('location:../docent');
 		die();
-	} else {
-		$check = 1;
 	}
 }
-if ($check == 1) {
-	$_SESSION['error'] = 'Inloggegevens onjuist.';
-}
+$_SESSION['error'] = 'Inloggegevens onjuist.';
 header('location:../login');
 die();
 ?>
