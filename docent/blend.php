b723bbd2d86a1a42585d2c936b419a5ec073f0b2
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-14 13:31:11 +0200

    fakfakfak

--- docent/blend.php
+++ docent/blend.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../assets/includes/conn.php');
 $pull = $conn->query('SELECT COUNT(naam) FROM leerling WHERE opleiding_ID = 1');
-echo $pull;
+while($row = $pull->fetch_assoc()) echo $row;
 
 ?>
