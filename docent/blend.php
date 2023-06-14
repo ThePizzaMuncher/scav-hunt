af77dcfc1995fc0eee55cf8854f02c4e5178f98a
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-14 13:39:20 +0200

    I hate SQL

--- docent/blend.php
+++ docent/blend.php
@@ -1,6 +1,7 @@
 <?php
 require_once('../assets/includes/conn.php');
-$pull = $conn->query('SELECT COUNT(naam) FROM leerling WHERE opleiding_ID = 1');
-echo $pull['COUNT(naam)'];
+$pull = $conn->query('SELECT COUNT(*) FROM leerling WHERE opleiding_ID = 1 as total from leerling');
+$data = $pull->fetch_assoc();
+echo $data['total'];
 
 ?>
