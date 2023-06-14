e3f56314597c15d344a5c6c0afcfda9077536e46
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-14 13:28:20 +0200

    zucht

--- docent/blend.php
+++ docent/blend.php
@@ -2,8 +2,5 @@
 require_once('../assets/includes/conn.php');
 $pull = $conn->query('SELECT COUNT(naam) FROM leerling WHERE opleiding_ID = 1');
 echo $pull;
-$lln;
-while($row = mysqli_fetch_assoc($pull)) {
-	array_push($lln, $row);
-} echo count($lln);
+
 ?>
