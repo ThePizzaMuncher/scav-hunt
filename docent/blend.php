ce43e278775bd7039e8bea05e7150b60fd5b57c4
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-14 13:51:22 +0200

    anticipation

--- docent/blend.php
+++ docent/blend.php
@@ -1,6 +1,7 @@
 <?php
 require_once('../assets/includes/conn.php');
-$pull = $conn->query('SELECT COUNT(naam) FROM leerling WHERE opleiding_ID = 1')->fetch_array();
-echo $pull[0];
+// $pull = 
+echo $conn->query('SELECT COUNT(naam) FROM leerling WHERE opleiding_ID = 1')->fetch_array()[0];
+// echo $pull[0];
 
 ?>
