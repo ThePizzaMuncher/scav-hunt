5b9e819c12411c03144bb5bc817ca6091c619944
ThePizzaMuncher <60735922+ThePizzaMuncher@users.noreply.github.com>
2023-06-15 11:13:24 +0200

    bon gionko

--- docent/blender.php
+++ docent/blender.php
@@ -5,57 +5,8 @@ if (!isset($_SESSION['docent'])) {
 } if (!$_SESSION['docent']) {
 	header('location:../login'); die();
 }
-
-require_once('../assets/includes/header.php');
 require_once('../assets/includes/conn.php');
-
-if(!isset($_POST)) {
-	echo <<< form
-	<head>
-	<title>Groepjes maken</title>
-	<script src="../assets/js/blender.js" defer></script>
-	</head>
-	<body id="pagina_blender">
-	<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
-		<section id="about" class="section-50 d-flex flex-column align-items-center">
-			<h2 class="panel-title">Blender</h2>
-			<form method="post" action="">
-				<h4 id="1">Aantal groepjes</h4>
-				<div class="input-group" id="ag">
-					<button type="button" class="minus-button" onclick="decrementValue(this)">-1</button>
-					<input type="number" name="ag" min="0" value="0">
-					<button type="button" class="plus-button" onclick="incrementValue(this)">+1</button>
-				</div>
-				<h4 id="2">Aantal mensen in groepje</h4>
-				<div class="input-group" id="amig">
-					<button type="button" class="minus-button" onclick="decrementValue(this)">-1</button>
-					<input type="number" name="amig" min="0" id="amig" value="0">
-					<button type="button" class="plus-button" onclick="incrementValue(this)">+1</button>
-				</div>
-				<button type="submit"><p>Genereer groepjes</p></button>
-			</form>
-		</section>
-	</section> <!-- End About Section -->
-
-	<script>//For value input form
-		function incrementValue(button) {
-			var input = button.parentNode.querySelector('input[type="number"]');
-			var value = parseInt(input.value);
-			input.value = value + 1;
-		}
-
-		function decrementValue(button) {
-			var input = button.parentNode.querySelector('input[type="number"]');
-			var value = parseInt(input.value);
-			if (value > 0) {
-				input.value = value - 1;
-			}
-		}
-	</script>
-	<!-- </body> -->
-	form;
-}
-else {
+if(isset($_POST)) {
 	$oID = $_SESSION['opleiding_ID']; $gID = 1; // opleiding_ID wordt meerdere keren gebruikt, dus een kortere variabelenaam is fijn
 	$conn->query('DELETE FROM groep WHERE groepsnaam != "standaard"'); // leeg de tabel met groepen
 	if(isset($_POST['ag'])) { // number of groups has been specified
@@ -80,8 +31,50 @@ else {
 		}
 	}
 }
+require_once('../assets/includes/header.php');
 ?>
+<head>
+<title>Groepjes maken</title>
+<script src="../assets/js/blender.js" defer></script>
+</head>
+<body id="pagina_blender">
+<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
+	<section id="about" class="section-50 d-flex flex-column align-items-center">
+		<h2 class="panel-title">Blender</h2>
+		<form method="post" action="">
+			<h4 id="1">Aantal groepjes</h4>
+			<div class="input-group" id="ag">
+				<button type="button" class="minus-button" onclick="decrementValue(this)">-1</button>
+				<input type="number" name="ag" min="0" value="0">
+				<button type="button" class="plus-button" onclick="incrementValue(this)">+1</button>
+			</div>
+			<h4 id="2">Aantal mensen in groepje</h4>
+			<div class="input-group" id="amig">
+				<button type="button" class="minus-button" onclick="decrementValue(this)">-1</button>
+				<input type="number" name="amig" min="0" id="amig" value="0">
+				<button type="button" class="plus-button" onclick="incrementValue(this)">+1</button>
+			</div>
+			<button type="submit"><p>Genereer groepjes</p></button>
+		</form>
+	</section>
+</section> <!-- End About Section -->
 
+<script>//For value input form
+	function incrementValue(button) {
+		var input = button.parentNode.querySelector('input[type="number"]');
+		var value = parseInt(input.value);
+		input.value = value + 1;
+	}
+
+	function decrementValue(button) {
+		var input = button.parentNode.querySelector('input[type="number"]');
+		var value = parseInt(input.value);
+		if (value > 0) {
+			input.value = value - 1;
+		}
+	}
+</script>
+</body>
 <?php
 $leerlingen = mysqli_query($conn, 'SELECT * FROM leerling');
 include "../assets/includes/footer.php";
