26d109059ee3d0a6f9685bb54c7f56cba21b9096
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-29 10:38:21 +0200

    header meta data update

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -21,6 +21,7 @@ $path = "~speurtocht/";
 	<link rel="icon" type="image/png" sizes="32x32" href="/' . $path . 'assets/img/favicon_io/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/' . $path . 'assets/img/favicon_io/favicon-16x16.png">
 	<link rel="manifest" href="/' . $path . 'assets/img/favicon_io/site.webmanifest">
+	<meta name="description" content="Speurtocht website. Speur je mee?">
 	';
 	?>
 
