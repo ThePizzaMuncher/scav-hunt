f6e56e2d438c796e9567de17b3cd77f6bcd74ce1
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-29 08:38:59 +0200

    Unieke code fix

--- docent/unieke_code_generatie.php
+++ docent/unieke_code_generatie.php
@@ -5,7 +5,7 @@ function random() {
     return rand(1, 9);
 }
 //Maak code en jaag in database
-$conn->query("UPDATE uniekecode SET code = " . random() . random() . random() . random() . " WHERE ID = 1");
+
 $code = 0;
 $pull = $conn->query("SELECT * FROM uniekecode WHERE ID = 1");
 //Laat code zien
@@ -13,7 +13,17 @@ echo '
 <section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
 	<section id="about" class="section-50 d-flex flex-column align-items-center">
     <h1>Code: ';
-    while ($row = $pull->fetch_assoc()) {echo $row["code"];}
+    while ($row = $pull->fetch_assoc()) {
+        $code = $row["code"];
+        if ($code == 0) {//Als code nog niet bestaat doe dan...
+            $conn->query("UPDATE uniekecode SET code = " . random() . random() . random() . random() . " WHERE ID = 1");
+            break;
+        }
+    }
+    if ($code == "0") {
+        $code = "Momenteel geen speurtocht.";
+    }
+    echo "<p>$code</p>";
     echo '
     </h1>
 </section>
