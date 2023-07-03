1826ec91c4c9c7be77650949d3fe1b5baebddb73
ValeraZSD <valerazsd@gmail.com>
2023-07-03 11:41:32 +0200

    Update send_vraag.php

--- vragen/send_vraag.php
+++ vragen/send_vraag.php
@@ -1,4 +1,4 @@
 <?php
 require_once("../assets/includes/conn.php");
-if (isset)
+if (isset($_POST["a"]) && isset($_POST["b"]) && isset($_POST["c"]) && isset($_POST["d"]) && isset($_POST["submit"]))
 ?>
