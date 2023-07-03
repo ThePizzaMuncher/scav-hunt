d8d7f1286a912bdb762f413171ed23218f2638dc
ValeraZSD <valerazsd@gmail.com>
2023-07-03 11:44:44 +0200

    234

--- vragen/send_vraag.php
+++ vragen/send_vraag.php
@@ -1,4 +1,21 @@
 <?php
 require_once("../assets/includes/conn.php");
-if (isset($_POST["a"]) && isset($_POST["b"]) && isset($_POST["c"]) && isset($_POST["d"]) && isset($_POST["submit"]))
+$a = false;
+$b = false;
+$c = false;
+$d = false;
+if (isset($_POST["submit"])) {
+    if (isset($_POST["a"])) {
+        $a = true;
+    } else if (isset($_POST["b"])) {
+        $b = true;
+    } else if (isset($_POST["b"])) {
+        $b = true;
+    } else if (isset($_POST["b"])) {
+        $b = true;
+    }
+
+} else {
+    die("Error: geen toegang!");
+}
 ?>
