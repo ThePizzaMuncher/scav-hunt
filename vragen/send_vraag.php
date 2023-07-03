8a56a117944c08056b374589cd715fb95de534ce
ValeraZSD <valerazsd@gmail.com>
2023-07-03 13:08:43 +0200

    Update send_vraag.php

--- vragen/send_vraag.php
+++ vragen/send_vraag.php
@@ -27,6 +27,7 @@ if (isset($_POST["submit"]) && isset($_POST["cvp1"]) && isset($_POST["rw"])) {
     $conn->query("UPDATE groep SET current_vraag = " . $cvp1 . " WHERE ID = " . $rw . ""); //Stel volgende vraag in.
     $rw -= 1; //Om current vraag ID te pakken.
     $conn->query("INSERT INTO antwoord(antwoorden, vraag_ID, groep_ID) VALUES ('$ia', $rw, $_SESSION[student_groepID])");
+    echo "<script>window.alert('Antwoord verzonden!')</script>";
 } else {
     die("Error: geen toegang!");
 }
