a94d6d809f3dd794b02e0044756bbbd8cc90b651
ValeraZSD <valerazsd@gmail.com>
2023-07-03 10:56:53 +0200

    23

--- docent/vragen-aanpassen.php
+++ docent/vragen-aanpassen.php
@@ -4,9 +4,12 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
 if (!isset($_SESSION['docent'])) {
-	header('location:../login'); die();
-} if ($_SESSION['docent'] != 1) {
-	header('location:../login'); die();
+	header('location:../login');
+	die();
+}
+if ($_SESSION['docent'] != 1) {
+	header('location:../login');
+	die();
 }
 /* if (isset($_SESSION["docent"])) {
 	if ($_SESSION["docent"] != 1) {
@@ -18,12 +21,13 @@ if (!isset($_SESSION['docent'])) {
 	die(); 
 }*/
 
-/*require_once("../assets/includes/header.php");*/
+require_once("../assets/includes/header.php");
 require_once("../assets/includes/conn.php");
 ?>
 
 
-<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
+<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset"
+	style="height: 100%;">
 	<section id="about" class="section-50 d-flex flex-column align-items-center">
 
 		<?php
@@ -39,8 +43,8 @@ require_once("../assets/includes/conn.php");
 			</div>
 
 		</div>
-		<?php 
-		
+		<?php
+
 		echo "<div class='scrollable'><table border='1' cellpadding='10'>";
 		echo "<thead>     
 			<tr>    
@@ -56,14 +60,14 @@ require_once("../assets/includes/conn.php");
 
 
 		$pull = $conn->query("SELECT vraag.antwoord,vraag.vragenlijst_ID,vraag.vraag,vragenlijst.ID,vragenlijst.docent_ID,docent.ID,docent.ID,docent.opleiding_ID,opleiding.ID,opleiding.opleiding_naam,vraag.ID FROM vraag INNER JOIN vragenlijst ON vraag.vragenlijst_ID = vragenlijst.ID INNER JOIN docent ON vragenlijst.docent_ID = docent.ID INNER JOIN opleiding ON docent.opleiding_ID = opleiding.ID");
-		while($row = $pull->fetch_assoc()) {
+		while ($row = $pull->fetch_assoc()) {
 			echo "<tr>";
 			echo "<td>$row[ID]</td>";
 			echo "<td>$row[vraag]</td>";
 			echo "<td>$row[antwoord]</td>";
 			echo "<td>$row[opleiding_naam]</td>";
 			echo "<td><a href=\"vragen_edit.php?id=$row[ID]\">Bewerk</a></td>";
-            echo "<td><a href=\"vragen_delete.php?id=$row[ID]\">Verwijder</a></td>";
+			echo "<td><a href=\"vragen_delete.php?id=$row[ID]\">Verwijder</a></td>";
 			echo "</tr>";
 		}
 
