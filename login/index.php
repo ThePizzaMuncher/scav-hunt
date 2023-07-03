1bd82c2abd9822de013630bbabb0d04ee0852edf
ValeraZSD <valerazsd@gmail.com>
2023-07-03 10:22:28 +0200

    Update index.php

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
 $_SESSION['pagina'] = 'login';
 require_once("../assets/includes/header.php");
 
-if (isset($_SESSION["error"])) {//Error feedback van login
+if (isset($_SESSION["error"])) { //Error feedback van login
   if ($_SESSION["error"] != 0) {
     echo "<script>
     window.alert($_SESSION[error]);
@@ -12,25 +12,49 @@ if (isset($_SESSION["error"])) {//Error feedback van login
 }
 
 if (isset($_SESSION['docent'])):
-	echo $_SESSION['docent'];?>
+  echo $_SESSION['docent']; ?>
   <script>location.replace("../docent/index.php") </script>.
 <?php endif; ?>
 
 <main id="main">
 
-<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
-  <section id="about" class="section-50 d-flex flex-column align-items-center">
-    <h3>Docent en admin login</h3>
-    <form method="post" action="send.php">
-      <input type="text" name="user" placeholder="Username" required>
-      <input type="password" name="pw" placeholder="Password" required>
-      <button type="submit" name="submit">Login</button>
-    </form>
-    <p id="error"></p>
+  <section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset"
+    style="height: 100%;">
+    <section id="about" class="section-50 d-flex flex-column align-items-center">
+
+      <div class="login-block">
+        <div class="container mlogin">
+          <div id="login">
+            <h1>Docent en admin login</h1>
+            <form action="" id="loginform" method="post" name="loginform">
+              <p>
+                <label for="user_login">User name<br>
+                  <input class="input form-control" name="user" size="32" type="text" value="" required>
+                </label>
+              </p>
+              <p>
+                <label for="user_pass">Password<br>
+                  <input class="input form-control" name="pw" size="32" type="password" value="" required>
+                </label>
+              </p>
+
+              <div class="text-center">
+                <button id="login" name="login" type="submit">Log In!</button>
+              </div>
+
+
+            </form>
+          </div>
+
+
+          </form>
+        </div>
+      </div>
+
+    </section>
   </section>
-</section>
 
 
-<?php
-require_once("../assets/includes/footer.php");
-?>
+  <?php
+  require_once("../assets/includes/footer.php");
+  ?>
