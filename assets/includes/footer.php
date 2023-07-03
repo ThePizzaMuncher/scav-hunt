8ee5fff780127b1e4dfec0001f1cf490e3cceb4a
ValeraZSD <valerazsd@gmail.com>
2023-07-03 09:20:12 +0200

    Update footer.php

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -17,19 +17,23 @@
 
 <a href="#" class="back-to-top x align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+<?php
+echo '
 <!-- Vendor JS Files -->
-<script src="../vendor/purecounter/purecounter_vanilla.js"></script>
-<script src="../vendor/aos/aos.js"></script>
-<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../vendor/glightbox/js/glightbox.min.js"></script>
-<script src="../vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="../vendor/swiper/swiper-bundle.min.js"></script>
-<script src="../vendor/typed.js/typed.min.js"></script>
-<script src="../vendor/waypoints/noframework.waypoints.js"></script>
-<script src="../vendor/php-email-form/validate.js"></script>
+<script src="/' . $path . 'assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="/' . $path . 'assets/vendor/aos/aos.js"></script>
+<script src="/' . $path . 'assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/' . $path . 'assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/' . $path . 'assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/' . $path . 'assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/' . $path . 'assets/vendor/typed.js/typed.min.js"></script>
+<script src="/' . $path . 'assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="/' . $path . 'assets/vendor/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
-<script src="../js/main.js"></script>
+<script src="/' . $path . 'assets/js/main.js"></script>
+';
+?>
 
 <style>
   .credits p {
