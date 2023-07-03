71897072da229ce6803a01a2ca9b8c7c87f9003c
ValeraZSD <valerazsd@gmail.com>
2023-07-03 09:51:06 +0200

    Update student_login.php

--- login/student_login.php
+++ login/student_login.php
@@ -1,23 +1,17 @@
 <?php
 require_once("../assets/includes/header.php");
 require_once("../assets/includes/conn.php");
-session_start();
 echo '
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
+<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset"
+    style="height: 100%;">
+    <section id="about" class="section-50 d-flex flex-column align-items-center">
     <form method="post" action="../assets/php/student_login_verw.php">
     <input type="text" name="code" required placeholder="Voer hier de code in">
     <input type="text" name="naam" required placeholder="Voer hier je naam in">
     <input type="submit" name="submit" value="Aanmelden">
     </form>
-</body>
-</html>
+</section>
+</section>
 ';
 if (isset($_SESSION["stl_fb"]) && $_SESSION["stl_fb"] != "0") { //Feedback van login actie
     echo "<script defer>
