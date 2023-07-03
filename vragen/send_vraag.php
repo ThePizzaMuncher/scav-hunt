62195fe931c96bf463c2005cfe1d5cfdde0df99f
ValeraZSD <valerazsd@gmail.com>
2023-07-03 12:50:17 +0200

    123

--- vragen/send_vraag.php
+++ vragen/send_vraag.php
@@ -5,27 +5,35 @@ $b = false;
 $c = false;
 $d = false;
 $counter = 0;
-if (isset($_POST["submit"])) {
+$cvp1 = 0;
+$rw = 0;
+$ia = 0;//Ingevoerde antwoord (String)
+if (isset($_POST["submit"]) && isset($_POST["cvp1"]) && isset($_POST["rw"])) {
+    $cvp1 = md5($_POST["cvp1"]);
+    $rw = md5($_POST["rw"]);
     if (isset($_POST["a"])) {
         $a = true;
+        $ia = "a";
         ++$counter;
     } else if (isset($_POST["b"])) {
         $b = true;
+        $ia = "b";
         ++$counter;
     } else if (isset($_POST["c"])) {
         $c = true;
+        $ia = "c";
         ++$counter;
     } else if (isset($_POST["d"])) {
         $d = true;
+        $ia = "d";
         ++$counter;
     }
     if ($counter != 1) {
         die("Error: meerdere antwoorden ingevuld of geen antwoorden ingevuld.");
     }
-    //volgende vraag instellen
-
-
-
+    $rw -= 1;//Om current vraag ID te pakken.
+    $conn->query("UPDATE groep SET current_vraag = " . $cvp1 . " WHERE ID = " . $rw . ""); //Stel volgende vraag in.
+    $conn->query("INSERT INTO antwoord(antwoorden, vraag_ID, groep_ID) VALUES ('$ia', , $_SESSION[student_groepID])")
 } else {
     die("Error: geen toegang!");
 }
