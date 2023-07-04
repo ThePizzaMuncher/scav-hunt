2e4a17c01d4266f4053e9cd876e4e0f615d534b3
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-04 15:10:49 +0200

    comments bij vragen

--- vragen/index.php
+++ vragen/index.php
@@ -103,9 +103,9 @@ if (isset($_SESSION["student_login"]) && $_SESSION["student_login"] == true && i
                 <input type='checkbox' name='d'>
                 <br>
                 <!--Invis values for questions-->
-                <input name='cvp1' value='" . ($row["current_vraag"] + 1) . "' style='display: none'>
-                <input name='rw' value='" . $row2["ID"] . "' style='display: none'>
-                <input name='gi' value='" . $row["ID"] . "' style='display: none'>
+                <input name='cvp1' value='" . ($row["current_vraag"] + 1)/*Meegeven van data naar send_vraag.php*/ . "' style='display: none'>
+                <input name='rw' value='" . $row2["ID"]/*Meegeven van data naar send_vraag.php*/ . "' style='display: none'>
+                <input name='gi' value='" . $row["ID"]/*Meegeven van data naar send_vraag.php*/ . "' style='display: none'>
                 <input type='submit' name='submit' value='Vraag inleveren'>
                 </form>";
             }
