e73643f1eb9809d8ef5146896c2f535afefd1acb
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-05 15:51:55 +0200

    update index.php

--- index.php
+++ index.php
@@ -6,70 +6,79 @@ echo <<<meta
 meta;
 ?>
 <style>
-  h2,
-  h3,
-  h4 {
-    text-align: center;
+  /* Video container styles */
+  .video-container {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-bottom: 56.25%; /* 16:9 aspect ratio */
+    overflow: hidden;
+    margin-bottom: 20px;
   }
 
-  p {
-    text-align: left;
+  .video-container video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 </style>
 
 
 <main id="main">
 
-<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
-  <section id="about" class="section-50 d-flex flex-column align-items-center">
-    <div class="container">
-      <div class="col-12">
-        <div class="full-width-block">
-          <?php
-          $filePath = "about.txt";
+  <section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
+    <section id="about" class="section-50 d-flex flex-column align-items-center">
+      <div class="container">
+        <div class="col-12">
+          <div class="full-width-block">
+            <?php
+            $filePath = "about.txt";
 
-          function formatText($text)
-          {
-            $text = preg_replace("/\*(.*?)\*/", "<em>$1</em>", $text);
-            $text = preg_replace("/\*\*(.*?)\*\*/", "<strong>$1</strong>", $text);
-            $text = preg_replace("/__(.*?)__/", "<u>$1</u>", $text);
+            function formatText($text)
+            {
+              $text = preg_replace("/\*(.*?)\*/", "<em>$1</em>", $text);
+              $text = preg_replace("/\*\*(.*?)\*\*/", "<strong>$1</strong>", $text);
+              $text = preg_replace("/__(.*?)__/", "<u>$1</u>", $text);
 
-            return $text;
-          }
-
-          $content = formatText(file_get_contents($filePath));
-
-          $lines = explode("\n", $content);
-          foreach ($lines as $line) {
-            if (preg_match("/^###/", $line)) {
-              echo "<h3>" . substr($line, 3) . "</h4>";
-            } elseif (preg_match("/^##/", $line)) {
-              echo "<h2>" . substr($line, 2) . "</h3>";
-            } elseif (preg_match("/^#/", $line)) {
-              echo "<h1>" . substr($line, 1) . "</h2>";
-            } else {
-              echo "<p>" . $line . "</p>";
+              return $text;
             }
-          }
-          ?>
-        </div><!--I pushed the project! ~wwwqr -->
+
+            $content = formatText(file_get_contents($filePath));
+
+            $lines = explode("\n", $content);
+            foreach ($lines as $line) {
+              if (preg_match("/^###/", $line)) {
+                echo "<h3>" . substr($line, 3) . "</h4>";
+              } elseif (preg_match("/^##/", $line)) {
+                echo "<h2>" . substr($line, 2) . "</h3>";
+              } elseif (preg_match("/^#/", $line)) {
+                echo "<h1>" . substr($line, 1) . "</h2>";
+              } else {
+                echo "<p>" . $line . "</p>";
+              }
+            }
+            ?>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <!-- Video loop -->
-    <div class="video-container">
-      <video autoplay loop muted>
-        <source src="assets/mp4/Leeuwarden.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>
-    </div>
+      <!-- Video loop -->
+      <div class="video-container">
+        <video autoplay loop muted>
+          <source src="assets/mp4/Leeuwarden.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </div>
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <div class="container">
-      <br>
-    </div>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <div class="container">
+        <br>
+      </div>
+    </section>
   </section>
-</section>
 
 
   <?php
