75f6a3fc8fa440cf7f09c0dbc3a1b5639d685f55
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-06 11:07:57 +0200

    ll_ig update

--- docent/ll_ig.php
+++ docent/ll_ig.php
@@ -7,17 +7,23 @@ require_once("../assets/includes/conn.php");
 <!-- Main part -->
 <section class="about d-flex justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
     <section id="about" class="section-50 d-flex align-items-center">
+        <div class="scrollable">
 <?php
 $pull = $conn->query("SELECT * FROM groep");
+echo "<table>";
 while ($row = $pull->fetch_assoc()) {
-    echo "<br>";
+    echo "<td>";
     $pull2 = $conn->query("SELECT * FROM leerling WHERE groep_ID = " . $row["ID"]);
     while ($row2 = $pull2->fetch_assoc()) {
-        echo "<br>" . $row2["naam"];
+        echo "<tr>";
+        echo "<p>" . $row2["naam"] . "</p>";
+        echo "</tr>";
     }
-    echo "<br>";
+    echo "</td>";
 }
+echo "</table>";
 ?>
+</div>
     </section>
 </section>
 
