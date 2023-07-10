89beab246f41fe8f0f29668af61bb28cc7963c24
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-10 19:35:33 +0200

    header bootstrap icon update

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -137,8 +137,8 @@ $path = "~speurtocht/";
 								echo '<li><a href="/' . $path . 'docent/groepen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
 								echo '<li><a href="/' . $path . 'docent/blender.php"><i class="bx bx-group"></i> Groepjes maken</a></li>';
 								echo '<li><a href="/' . $path . 'vragen/tussenstand"><i class="bx bx-group"></i> Groepjes tussenstand</a></li>';
+								echo '<li><a href="/' . $path . 'docent/koter_analyzer.php"><i class="bx bx-map"></i> Groepjes locatie</a></li>';
 								echo '<li><a href="/' . $path . 'docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
-								echo '<li><a href="/' . $path . 'docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
 								echo '<li><a href="/' . $path . 'docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
 								echo '<li><a href="/' . $path . 'docent/unieke_code_generatie.php"><i class="bx bx-code"></i> Code genereren</a></li>';
 								if (isset($_SESSION['admin']))
