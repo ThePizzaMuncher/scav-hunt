df3fd82fe6146a3e49f3305ca9a26c2019d3c2bd
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-11 17:07:56 +0200

    Update docent.js

--- assets/js/docent.js
+++ assets/js/docent.js
@@ -69,6 +69,7 @@ setInterval(() => {//Update de map voor de docent om de aangegeven seconden.
     }});
     let dataArr = output.split("(_)");
     dataArr.forEach((s) => {//Voor elk groepje doe...
+        console.log(s);
         let Ix = 0;
         let Iy = 0;
         let Iz = 0;
@@ -77,7 +78,6 @@ setInterval(() => {//Update de map voor de docent om de aangegeven seconden.
         let current_vraag = innerOutput[1];
         let score = innerOutput[2];
         let ID = innerOutput[3];
-        console.log(ID);
         switch (current_vraag) {//Pak coordinaten bij passende qr-code.
             case 0:
                 Ix = xD;
