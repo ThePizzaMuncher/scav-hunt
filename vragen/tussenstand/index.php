993272001f15e64b6ff0d4ea5c4dbc82b2ccdd0d
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-11 10:26:56 +0200

    tip update

--- vragen/tussenstand/index.php
+++ vragen/tussenstand/index.php
@@ -49,7 +49,7 @@ body {
 ";
 $opleidingID = $_SESSION['opleiding_ID'];
 $DCN = $_SESSION['naam'];
-$pull = $conn->query("SELECT * FROM groep WHERE docent_ID = (SELECT ID FROM docent WHERE opleiding_ID = $opleidingID AND naam = $DCN)");
+$pull = $conn->query("SELECT * FROM groep WHERE docent_ID = (SELECT ID FROM docent WHERE opleiding_ID = $opleidingID AND naam = '$DCN')");
 echo "<div class='gordel' style='margin-top: -5vw;'><p class='txt'>Tussenstand groepjes</p></div>";
 echo "<div class='gordel'>";
 while ($row = $pull->fetch_assoc()) {
