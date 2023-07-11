72f11b6bff602c233fdcfd6d7cdb057205e89136
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-11 11:38:40 +0200

    koter analyzer update

--- docent/koter_analyzer.php
+++ docent/koter_analyzer.php
@@ -22,11 +22,6 @@ if (!isset($_SESSION['docent'])) {
 <section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
     <section id="about" class="section-50 d-flex flex-column align-items-center">
         <div class="docent-information">
-            <p>Naam: <span id="name"></span></p>
-            <p>Nummer: <span id="number"></span></p>
-            <p>x: <span id="x"></span></p>
-            <p>y: <span id="y"></span></p>
-            <p>Datum: <span id="date"></span></p>
             <div id="map"></div>
         </div>
     </section>
