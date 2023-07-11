d3df1de6e6caed7f3e1f99167973a355aa1d7800
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-11 20:07:07 +0200

    koter analyzer, leaflet.css update

--- docent/koter_analyzer.php
+++ docent/koter_analyzer.php
@@ -16,13 +16,14 @@ if (!isset($_SESSION['docent'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script><!-- Ajax -->
     <script src="../assets/js/docent.js" defer></script>
     <link rel="stylesheet" href="../assets/css/koter_analyzer.css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <link rel="stylesheet" href="../assets/css/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <link rel="stylesheet" href="../assets/css/style.css">
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </head>
 <section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
     <section id="about" class="section-50 d-flex flex-column align-items-center">
         <div class="docent-information">
-            <div id="map"></div>
+            <div id="map" class="mapvoorkoters"></div>
         </div>
     </section>
 </section>
