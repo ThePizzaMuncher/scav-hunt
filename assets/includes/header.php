27fd0303b850492dda45f3c7ad8ba89c0573c2cb
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-11 10:54:48 +0200

    header update

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -149,6 +149,7 @@ $path = "~speurtocht/";
 								// bar;
 							} else if (isset($_SESSION["student_login"]) && $_SESSION["student_login"]) {
 								echo '<li><a href="/' . $path . 'vragen/tussenstand" class="nav-link scrollto hover-sound"><i class="bx bx-group"></i> Tussenstand</a></li>';
+								echo '<li><a href="/' . $path . 'vragen/tip" class="nav-link scrollto hover-sound"><i class="bx bx-question"></i> Hint\Tip</a></li>';
 							}
 							else if (!isset($_SESSION['pagina'])) {
 								echo '<li><a href="/' . $path . 'login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
