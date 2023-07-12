296abeae184d7b37d26169fe5f65bbbac6d2715b
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-12 12:39:12 +0200

    inport update && header

--- docent/IMPORT.php
+++ docent/IMPORT.php
@@ -1,10 +1,11 @@
 <?php
 // Load the database configuration file
 include_once '../assets/includes/conn.php';
+include_once '../assets/includes/header.php';
 
 // Get status message
-if(!empty($_GET['status'])){
-    switch($_GET['status']){
+if (!empty($_GET['status'])) {
+    switch ($_GET['status']) {
         case 'succ':
             $statusType = 'alert-success';
             $statusMsg = 'Members data has been imported successfully.';
@@ -25,13 +26,18 @@ if(!empty($_GET['status'])){
 ?>
 
 <!-- Display status message -->
-<?php if(!empty($statusMsg)){ ?>
-<div class="col-xs-12">
-    <div class="alert <?php echo $statusType; ?>"><?php echo $statusMsg; ?></div>
-</div>
-<?php } ?>
-    <!-- CSV file upload form -->
+<?php if (!empty($statusMsg)) { ?>
+    <section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
+        <section id="about" class="section-50 d-flex flex-column align-items-center">
+            <div class="col-xs-12">
+                <div class="alert <?php echo $statusType; ?>"><?php echo $statusMsg; ?></div>
+            </div>
+        <?php } ?>
+        <!-- CSV file upload form -->
         <form action="UPLOADEN.php" method="post" enctype="multipart/form-data">
             <input type="file" name="file" />
             <input type="submit" name="importSubmit" value="IMPORT">
         </form>
+        </section>
+    </section>
+<?php include_once '../assets/includes/header.php'; ?>
