86ff2535421eab712b89e3dfa047786f288e96c3
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-12 09:40:45 +0200

    tip update

--- vragen/tip/index.php
+++ vragen/tip/index.php
@@ -59,51 +59,18 @@ switch ($current_vraag) {
         $tipTxt = "Je hebt de speurtocht afgerond! Vraag de desbetreffende docent voor de winnaars.";
     break;
 }
-require_once("../../assets/includes/header.php");
-/*echo '
-<style>
-    .gordel {
-        width: 100%;
-        height: 10vw;
-        align-items: center;
-        justify-content: center;
-        display: flex;
-        text-align: center;
-    }
-    .txt {
-        color: black;
-        text-shadow: 0.2vw 0.2vw 0.2vw grey;
-        font-size: 2vw;
-    }
-</style>
-<div class="gordel">
-<p class="txt">' . $tipTxt . '</p>
-</div>
-';*/
-echo '
+require_once("../../assets/includes/header.php"); ?>
+<section class="about d-flex flex-column justify-content-center align-items-center sticked-header-offset" style="height: 100%;">
+	<section id="about" class="section-50 d-flex flex-column align-items-center">
 <div class="container">
   <div class="row justify-content-center align-items-center" style="margin-top: 5vw; margin-bottom: 5vw;">
     <form class="col-12">
-      <div class="form-group"><h3>' . $tipTxt . '</h3></div>
+      <?php echo '<div class="form-group"><h3>' . $tipTxt . '</h3></div>'; ?>
     </form>   
   </div>
 </div>
-';
-/*
-<section id="about" class="about">
-    <div class="container" style="overflow: hidden;">
-        <div class="row">
-          <div class="col-lg-4 justify-content-center align-items-center sticked-header-offset">
-            <div class="section-title">
-            </div>
-          </div>
-          <div class="col-lg-8 pt-4 pt-lg-0 content">
-            <p>
-            </p>
-          </div>
-        </div>
-      </div>
     </section>
-*/
+    </section>
+<?php
 require_once("../../assets/includes/footer.php");
 ?>
