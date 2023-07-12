879fa491a5e87fe811f3a92f803fa09d70853b83
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-12 12:45:54 +0200

    ts update

--- vragen/tussenstand/index.php
+++ vragen/tussenstand/index.php
@@ -142,7 +142,7 @@ $pull = $conn->query($query);
         <?php 
         $isMobile = isMobileDevice(); // Check if the device is mobile
         if ($isMobile) {
-            echo "<p style='margin-top: 50px;'>Omdrijen uw telefoon naar landschape modus voor betere kijken</p>";
+            echo "<p style='margin-top: 50px;'>Zet je apparaat in de horizontale stand voor een betere weergave.</p>";
         }
         ?>
     </section>
