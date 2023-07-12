a8ad2ee46685d901d0a8f8d56c24b092c607a2c0
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-12 08:59:39 +0200

    Update docent.js

--- assets/js/docent.js
+++ assets/js/docent.js
@@ -2,7 +2,7 @@ let x = 0;
 let y = 0;
 let z = 0;
 let xD = 53.203156539719096; let yD = 0; let zD = 5.778884175278147;//Default coords
-let schaal = 15;
+let schaal = 14;
 let map = L.map('map').setView([x, z], schaal);
 let delay = 10;//Update delay in seconden.
 
