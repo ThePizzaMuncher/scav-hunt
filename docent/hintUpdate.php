5502e951f71b5bcd15e76ab4eeee946400ac000a
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-20 20:41:50 +0200

    Create hintUpdate.php

---
+++ docent/hintUpdate.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if (!isset($_SESSION['docent']) || $_SESSION['docent'] != 1) {
+    echo "<script>
+    window.alert('Error: geen toegang.');
+    window.open('../', '_self');
+    </script>";
+    die();
+}
+if (!isset($_POST["id"]) || !isset($_POST["hint"]) || !isset($_POST["submit"])) {
+    echo "<script>
+    window.alert('Error: Niet alle velden zijn ingevoerd.');
+    window.open('../', '_self');
+    </script>";
+    die();
+}
+$hintID = $_POST["id"];
+$hintNew = $_POST["hint"];
+$conn->query("UPDATE hint SET tip = " . $hintNew . " WHERE hintID = " . $hintID);
+header("location: ./hints.php");
+?>
