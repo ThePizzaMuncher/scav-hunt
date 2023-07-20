ba02e96828062b4d038c9619669a64a42645f494
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-20 18:15:44 +0200

    Update hints.php

--- docent/hints.php
+++ docent/hints.php
@@ -12,7 +12,7 @@ require_once("../assets/includes/header.php");
 $dop = $_SESSION['opleiding_ID'];//Docent Opleiding ID
 $pull = $conn->query("SELECT * FROM hint WHERE opleidingID = $dop");
 while ($row = $pull->fetch_assoc()) {
-    echo $row["tip"] . " <a href='hintEdit.php/?id=" . $row["hintID"] . "'>Bewerk</a><br>";
+    echo "<p>" . $row["tip"] . "</p>" . " <a href='hintEdit.php/?id=" . $row["hintID"] . "'>Bewerk</a><br>";
 }
 require_once("../assets/includes/footer.php");
 ?>
