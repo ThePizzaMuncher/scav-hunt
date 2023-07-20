f82b2a5f48b1c1051de57e767610cc50d34ae3cf
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-20 15:57:27 +0200

    hints.php login check update

--- docent/hints.php
+++ docent/hints.php
@@ -1,3 +1,13 @@
 <?php
+session_start();
+if (!isset($_SESSION['docent']) || $_SESSION['docent'] != 1) {
+    echo "<script>
+    window.alert('Error. geen toegang!');
+    window.open('../', '_self');
+    </script>";
+    die();
+}
 require_once("../assets/includes/header.php");
+
+require_once("../assets/includes/footer.php");
 ?>
