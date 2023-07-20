0555d2f974d3a78e2b7baae9fce846740e16c978
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-20 19:41:36 +0200

    Update hintEdit.php

--- docent/hintEdit.php
+++ docent/hintEdit.php
@@ -11,7 +11,7 @@ require_once("../assets/includes/conn.php");
 $hintID = $_GET["id"];
 $pull = $conn->query("SELECT hintID FROM hint WHERE hintID = $hintID AND opleidingID = " . $_SESSION['opleiding_ID']);
 $output = $pull->fetch_assoc();
-echo $output;
+echo $output["hintID"];
 require_once("../assets/includes/header.php");
 $pull;
 require_once("../assets/includes/footer.php");
