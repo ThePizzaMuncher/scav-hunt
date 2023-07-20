cdf76bc04960b8179ded23cdf751119bafd1b51c
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-07-20 15:26:37 +0200

    leerlingen verwijderen inplaats van hele groep

--- docent/index.php
+++ docent/index.php
@@ -70,7 +70,7 @@ require_once("../assets/includes/conn.php");
 						window.open(document.URL + 'delete.php?id=' + id, '_self');
 					}
 					</script>";
-					if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {//Admin verweidert alle leerlingen van alle opleidingen.
+					/*if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {//Admin verweidert alle leerlingen van alle opleidingen.
 						$padNaarDel = "test1";
 					}
 					else {
@@ -82,7 +82,7 @@ require_once("../assets/includes/conn.php");
 						if (vr) {
 							window.open('../" . $padNaarDel . "', '_self');
 						}
-					}
+					}*/
 					</script>";
 				while ($row = $pull->fetch_assoc()) {
 					echo "<tr>";
